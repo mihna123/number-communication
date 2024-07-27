6b7de6d6cb7dba906b4ab83dbb857b33dd13c47b
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-07-27 17:17:17 +0200

    Connect to atlas

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,11 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 import { ExpressAuth } from '@auth/express';
 import Google from "@auth/express/providers/google";
 import * as PostsService from './services/post.service.mjs'
 
 const app = express();
 const port = 3000;
+const mongoUri = process.env.MONGO_URI;
+
+const clientOptions = {
+    serverApi: {
+        version: '1',
+        strict: true,
+        deprecationErrors: true
+    }
+};
+try {
+    await mongoose.connect(mongoUri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+} catch (e) {
+    await mongoose.disconnect();
+    console.log(e);
+}
 
 app.use(express.json());
 app.use('/auth/*', ExpressAuth({ providers: [Google] }))
