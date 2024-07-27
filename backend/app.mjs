77e3232753517284b27a6210a4a729b645229f8c
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-07-27 16:12:35 +0200

    Add auth and dotenv packages

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,10 +1,14 @@
 import express from 'express';
+import { ExpressAuth } from '@auth/express';
+import Google from "@auth/express/providers/google";
 import * as PostsService from './services/post.service.mjs'
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use('/auth/*', ExpressAuth({ providers: [Google] }))
 
 app.get('/posts', async (_, res) => {
     const posts = await PostsService.getAllPosts();
